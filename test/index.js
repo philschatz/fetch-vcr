a7a555ec4fa1219650045ee30a58d77d3afedbd2
Alan Tirado <atirado0617@gmail.com>
2019-01-15 15:53:07 -0800

    fixing long fixture names by hashing query params (#32)

--- test/index.js
+++ test/index.js
@@ -2,6 +2,10 @@ import test from 'ava'
 import assert from 'assert'
 import fetchVCR from '../lib/index'
 
+function createLongParams (num) {
+  return 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter => `${letter}=${num}`).join('&')
+}
+
 test.before(t => {
   fetchVCR.configure({
     mode: 'playback',
@@ -12,55 +16,55 @@ test.before(t => {
 
 test('ignores urls in ignoreUrls', t => {
   return fetchVCR('https://test.com?ignore=true')
-  .then(response => {
-    return response.text()
-    .then(text => t.pass())
-  })
+    .then(response => {
+      return response.text()
+        .then(text => t.pass())
+    })
 })
 
 test('fetches from the fixture', t => {
   // fetchVCR.configure({mode: 'record'})
   return fetchVCR('https://cnx.org?') // the `?` is to make sure the fixture filename is escaped properly
-  .then(response => {
-    return response.text()
-    .then(text => t.pass())
-  })
+    .then(response => {
+      return response.text()
+        .then(text => t.pass())
+    })
 })
 
 test('caches a 404 page', t => {
   // fetchVCR.configure({mode: 'record'})
   return fetchVCR('https://google.com/404')
-  .then(response => {
-    return response.text()
-    .then(text => t.pass())
-  })
+    .then(response => {
+      return response.text()
+        .then(text => t.pass())
+    })
 })
 
 test('saves JSON properly', t => {
   // fetchVCR.configure({mode: 'record'})
   // return fetchVCR('https://api.github.com/search/repositories?q=octokat')
   return fetchVCR('https://archive.cnx.org/contents/9a1df55a-b167-4736-b5ad-15d996704270%403.2.json')
-  .then(response => {
-    return response.json()
-    .then(text => t.pass())
-  })
+    .then(response => {
+      return response.json()
+        .then(text => t.pass())
+    })
 })
 
 test('can recover from invalid JSON', t => {
   return fetchVCR('https://example.com/invalid-because-of-extra.json')
-  .then(response => {
-    return response.json()
-    .then(text => t.pass())
-  })
+    .then(response => {
+      return response.json()
+        .then(text => t.pass())
+    })
 })
 
-test('caches the fixture', async t => {
-  const TEST_URL = 'https://openstax.org'
+test('caches fixture with long name', async t => {
+  const TEST_URL = `https://openstax.org?${createLongParams(1)}&${createLongParams(2)}`
   // Verify that the fixture does not yet exist
   fetchVCR.configure({mode: 'playback'})
   try {
     await fetchVCR(TEST_URL)
-    t.fail('This URL Should not initially be in the fixtures directory')
+    t.fail(`This URL Should not initially be in the fixtures directory ${TEST_URL}`)
   } catch (e) {
     assert(e)
   }
@@ -95,5 +99,4 @@ test.cb('runs in jsdom', t => {
       window.fetch = fetchVCR
     }
   })
-
 })
